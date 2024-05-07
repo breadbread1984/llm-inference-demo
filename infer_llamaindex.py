fd2fd27fc8cb587889bd4e994089b2d4220099da
breadbread1984 <breadbread1984@163.com>
2024-05-07 10:01:02 +0800

    update

--- infer_llamaindex.py
+++ infer_llamaindex.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 
+import json
 from absl import flags, app
+from huggingface_hub import login
 from transformers import AutoTokenizer
-from llama_index.llms.huggingface import HuggingFaceInferenceAPI
+from llama_index.llms.huggingface import HuggingFaceLLM
 
 FLAGS = flags.FLAGS
 
 def add_options():
-  pass
+  flags.DEFINE_string('prompt', default = None, help = 'path to file containning prompts, line break is replaced by \\n')
+  flags.DEFINE_string('output', default = 'outputs.json', help = 'path to output file')
 
 def main(unused_argv):
+  login('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')
   tokenizer = AutoTokenizer.from_pretrained('meta-llama/Meta-Llama-3-8B-Instruct', trust_remote_code = True)
   def messages_to_prompt(message):
     messages = [{'role': message.role, 'content': message.content}]
@@ -22,13 +26,17 @@ def main(unused_argv):
   llm = HuggingFaceLLM(
     model_name = 'meta-llama/Meta-Llama-3-8B-Instruct',
     tokenizer_name = 'meta-llama/Meta-Llama-3-8B-Instruct',
-    generate_kwargs = {'temperature': 0.6, 'top_p': 0.9, 'do_sample': True},
+    generate_kwargs = {'temperature': 0.6, 'top_p': 0.9, 'do_sample': True, 'use_cache': True},
     messages_to_prompt = messages_to_prompt,
     completion_to_prompt = completion_to_prompt,
-    token = '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',
     device_map = "auto")
-  response = llm.chat('how is the day?')
-  print(response)
+  outputs = list()
+  with open(FLAGS.prompt, 'r') as f:
+    for line in f.readlines():
+      response = llm.complete(line.replace('\\n','\n'))
+      outputs.append(str(response))
+  with open(FLAGS.output, 'w') as f:
+    f.write(json.dumps(outputs))
 
 if __name__ == "__main__":
   add_options()
